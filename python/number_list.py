0c3d1a26feeb38b9fd3ab05335c53bd993dae81a
Paul Dziedzic <pfdj7@yahoo.com>
2019-08-27 15:12:44 -0400

    Added new sorting methods August 27, 2019 By Paul Dziedzic

--- python/number_list.py
+++ python/number_list.py
@@ -1,7 +1,8 @@
 # Paul Dziedzic
 # August 26, 2019
+# Last updated August 27, 2019
 
-# Create a list class 'number_list' with a function to swap every other element
+# Class number list that can be sorted by multiple different sorting algorithms
 
 class number_list:
 	def __init__(self, list):
@@ -14,7 +15,7 @@ class number_list:
 				self.list[counter], self.list[counter-1] = self.list[counter-1], self.list[counter]
 			counter += 1
 
-	def sortList(self):
+	def selectSort(self):
 		for i in range(len(self.list)):
 			min = i
 			
@@ -23,9 +24,60 @@ class number_list:
 					min = j
 			
 			self.list[i],self.list[min] = self.list[min],self.list[i]
+	
+	def partition(self,arr,low,high):
+		i = low-1
+		pivot = arr[high]
+		
+		for j in range(low,high):
+			if arr[j] < pivot:
+				i += 1
+				arr[i],arr[j] = arr[j],arr[i]
+				
+		arr[i+1],arr[high] = arr[high],arr[i+1]
+		return i+1
+	
+	def quickSort(self,arr,low,high):
+		if low < high:
+			p = self.partition(arr,low,high)
+			self.quickSort(arr,low,p-1)
+			self.quickSort(arr,p+1,high)
+			
+	def mergeSort(self,arr):
+		if len(arr) > 1:
+			mid = len(arr) // 2
+			left = arr[:mid]
+			right = arr[mid:]
+			
+			self.mergeSort(left)
+			self.mergeSort(right)
+			
+			i = j = k = 0
+			
+			while i < len(left) and j < len(right):
+				if left[i] < right[j]:
+					arr[k] = left[i]
+					i += 1
+				else:
+					arr[k] = right[j]
+					j += 1
+				k += 1
+				
+			while i < len(left):
+				arr[k] = left[i]
+				i += 1
+				k += 1
+				
+			while j < len(right):
+				arr[k] = right[j]
+				j += 1
+				k += 1
 			
 num = number_list([4,2,7,5,1,2,9,7]) # Create a new list
 num.swapSecond() # Swap every other index
 print(num.list) # Prints [2,4,5,7,2,1,7,9]
-num.sortList() # Sort array from least to greatest
+size = len(num.list)
+num.mergeSort(num.list) # Merge sort array from least to greatest
 print(num.list) # Prints [1,2,2,4,5,7,7,9]
+
+# print(list(reversed(num.list))) # Reversed list
