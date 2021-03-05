6e5adca570ba2e9003fffe2b4f3dbb0c8ed144c0
Paul Dziedzic <53009545+pauld4@users.noreply.github.com>
2021-03-04 23:55:54 -0500

    Update banking.cpp

--- c++/banking.cpp
+++ c++/banking.cpp
@@ -1,6 +1,6 @@
 //============================================================================
-// Name        : HelloWorld.cpp
-// Author      : 
+// Name        : banking.cpp
+// Author      : Paul Dziedzic
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
